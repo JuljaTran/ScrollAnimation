94331277d182778d0e138a80d90e7f4d4d299712
JuljaTran <s2310238071@fhooe.at>
2026-01-05 13:52:39 +0100

    Projekt aufsetzen

---
+++ Animations/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Scroll-Animation test environment</h1>
+        <section className="section">Start </section>
+        <section className="section">Scroll</section>
+        <section className="section">End</section>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ notes.md
@@ -0,0 +1,6 @@
+Änderungen
+    Was
+    Wann 
+    Wieso
+
+Sections angelegt um nachvollziehen zu können von wo bis wo die Scrollanimation geht
